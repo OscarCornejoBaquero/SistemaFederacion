2cdfe1879bf23042668e6e5eb2431815571f90e8
Victorpiedrab <victor.piedrab@ug.edu.ec>
2021-08-17 21:18:31 -0500

    Tutoria Victor Piedra

--- Test/UsuariosTest.php
+++ Test/UsuariosTest.php
@@ -11,9 +11,9 @@ class UsuariosTest extends TestCase
     public function testGetUsuario()
     {
 
-        $usuario = new Usuarios();
-        $this->asserEqueals('{"idpersona":"27","cedula":"0955489546","nombres":"Felipe","apellidos":"Mirabá","telefono":"988659003","email_user":"felipe2@hotmail.com","id_rol":"4","nombre_rol":"Usuario Partido","status":"1","fechaRegistro":"11-07-2021"}}
-        ', $usuario->getUsuario(27));
+        $usuarioPrueba = new Usuarios();
+        $this->asserEqueals('{"idpersona":"27","cedula":"0955489546","nombres":"Felipe","apellidos":"Mirabá","telefono":"988659003","email_user":"felipe2@hotmail.com","id_rol":"4","nombre_rol":"Usuario Partido","status":"1","fechaRegistro":"12-08-2021"}}
+        ', $usuarioPrueba->getUsuario(27));
 
     }
 }
